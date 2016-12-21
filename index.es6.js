92f03d4875dfba701ed58f01c2eb519a72d1bdee
Igor Novak <igor.novak@emesa.nl>
2016-12-21 16:00:22 +0100

    Exclude all exports except "isValidNumber"

--- index.es6.js
+++ index.es6.js
@@ -1,14 +1,14 @@
-export
-{
-	default as parse
-}
-from './source/parse'
+// export
+// {
+// 	default as parse
+// }
+// from './source/parse'
 
-export
-{
-	default as format
-}
-from './source/format'
+// export
+// {
+// 	default as format
+// }
+// from './source/format'
 
 export
 {
@@ -18,9 +18,9 @@ export
 from './source/validate'
 
 
-export
-{
-	default as as_you_type,
-	default as asYouType
-}
-from './source/as you type'
+// export
+// {
+// 	default as as_you_type,
+// 	default as asYouType
+// }
+// from './source/as you type'
